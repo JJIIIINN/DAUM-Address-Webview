d6afff9d9113e0b2ded07c2c3ea048decca6596d
JJIIIINN <kgb21537@gmail.com>
2023-12-16 01:36:07 +0900

    fix: json을 string 형태로 변환 후 전송

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ function App() {
     return (
         <DaumPostcode
             style={{ width: "100vw", height: "100vh" }}
-            onComplete={() => {
+            onComplete={(data) => {
                 if (/Mobi/i.test(window.navigator.userAgent)) {
                     if (/iPhone|iPad|iPod/i.test(navigator.userAgent))
                         globalThis.webkit.messageHandlers.postData.postMessage(
-                            'data'
+                            data.toString()
                         );
                     else {
-                        globalThis.webview.postMessage('data');
+                        globalThis.webview.postMessage(data.toString());
                     }
                 }
             }}
