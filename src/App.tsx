fff2c59ab7a66a84e508838a5f9cb3efde7f3f6f
JJIIIINN <kgb21537@gmail.com>
2023-12-26 01:29:05 +0900

    fix: 원상복구

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
             onComplete={(data) => {
                 if (mobile) {
                     if (
-                        /iphone|ipad|ipod|mac/i.test(
+                        /iphone|ipad|ipod/i.test(
                             navigator.userAgent.toLowerCase()
                         )
                     )
@@ -21,6 +21,10 @@ function App() {
                     else {
                         globalThis.Android.postAddress(JSON.stringify(data));
                     }
+                } else {
+                    globalThis.webkit.messageHandlers.callBackHandler.postMessage(
+                        data
+                    );
                 }
             }}
         />
